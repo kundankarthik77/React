68343b0ee15117cc28a79144c0aef0cb765606eb
Kundan Keshidi <kundankarthik77@gmail.com>
2021-03-31 00:56:43 -0500

    handled page changes to highlight the current page in pagination

--- first-app/counter-app/vidly/src/components/common/pagination.jsx
+++ first-app/counter-app/vidly/src/components/common/pagination.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import _ from "lodash";
 
 const Pagination = (props) => {
-  const { itemsCount, pageSize } = props;
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+  console.log(currentPage);
 
   const pagesCount = Math.ceil(itemsCount / pageSize);
-  console.log(pagesCount);
   if (pagesCount == 1) return null;
   const pages = _.range(1, pagesCount + 1);
 
@@ -13,8 +13,13 @@ const Pagination = (props) => {
     <nav>
       <ul className="pagination">
         {pages.map((page) => (
-          <li key={page} className="page-item">
-            <a className="page-length">pages</a>
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a className="page-length" onClick={() => onPageChange(page)}>
+              {pages}
+            </a>
           </li>
         ))}
       </ul>
