08817bcf3cdb0a9a020de8f6c74a8d4575a74ab3
Kundan Keshidi <kundankarthik77@gmail.com>
2021-03-31 17:17:13 -0500

    Implementing filtering for Action, Comedy, Thriller

--- first-app/counter-app/vidly/src/components/movies.jsx
+++ first-app/counter-app/vidly/src/components/movies.jsx
@@ -41,26 +41,37 @@ class Movies extends Component {
   };
 
   handleGenreSelect = () => {
-    console.log("Genre Select");
+    this.setState({ selectedGenre: genre });
   };
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies: allMovies } = this.state;
+    const {
+      pageSize,
+      currentPage,
+      selectedGenre,
+      movies: allMovies,
+    } = this.state;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
-    const movies = paginate(allMovies, currentPage, pageSize);
+    const filtered = selectedGenre
+      ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+      : allMovies;
+
+    const movies = paginate(filtered, currentPage, pageSize);
+
     return (
       <div className="row">
         <div className="col-3">
           <ListGroup
             items={this.state.genre}
+            selectedItem={this.state.selectedGenre}
             onItemSelect={this.handleGenreSelect}
           />
         </div>
         <div className="col">
-          <p>Showing {count} movies in the database</p>
+          <p>Showing {filtered.length} movies in the database</p>
           <table className="table">
             <thead>
               <tr>
@@ -98,7 +109,7 @@ class Movies extends Component {
             </tbody>
           </table>
           <Pagination
-            itemsCount={count}
+            itemsCount={filtered.length}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={this.handlePageChange}
