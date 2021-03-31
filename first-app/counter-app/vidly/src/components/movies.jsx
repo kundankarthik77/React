da28ea0024ecd7141376aa69174642181623679a
Kundan Keshidi <kundankarthik77@gmail.com>
2021-03-31 17:22:18 -0500

    Adding all genres and resetting genres current page: 1

--- first-app/counter-app/vidly/src/components/movies.jsx
+++ first-app/counter-app/vidly/src/components/movies.jsx
@@ -15,7 +15,9 @@ class Movies extends Component {
   };
 
   componentDidMount() {
-    this.setState({ movies: getMovies(), genres: getGenres() });
+    const genres = [{ name: "All Genres" }, ...getGenres()];
+
+    this.setState({ movies: getMovies(), genres });
   }
   // constructor(){
   //     super();
@@ -41,7 +43,7 @@ class Movies extends Component {
   };
 
   handleGenreSelect = () => {
-    this.setState({ selectedGenre: genre });
+    this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
   render() {
@@ -55,9 +57,10 @@ class Movies extends Component {
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
-    const filtered = selectedGenre
-      ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
-      : allMovies;
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+        : allMovies;
 
     const movies = paginate(filtered, currentPage, pageSize);
 
