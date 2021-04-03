de072707feb1855f2bfb48cbe744ae5018b2f164
Kundan Keshidi <kundankarthik77@gmail.com>
2021-04-03 17:17:06 -0500

    validate form using Joi

--- first-app/counter-app/vidly/src/components/loginForm.jsx
+++ first-app/counter-app/vidly/src/components/loginForm.jsx
@@ -16,26 +16,33 @@ class LoginForm extends Component {
   // this schema can be seen in npm joi on browser search
   // syntax helps me understand how validations are created
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required(),
+    username: Joi.string().required().label('Username'),
+    password: Joi.string().required().label('Password'),
   };
 
   validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
-      abortEarly: false,
-    });
-    console.log(result);
+    const options = { abortEarly: false};
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+
+    if (!error) return null;
 
     const errors = {};
 
-    const { account } = this.state;
+    for(let item of result.error.details) 
+       errors[item.path[0]] = item.message;
+    return errors;
+    // console.log(result);
 
-    if (account.username.trim() === "")
-      errors.username = "Username is required.";
-    if (account.password.trim() === "")
-      errors.password = "Password is required.";
+    // const errors = {};
 
-    return Object.keys(errors).length === 0 ? null : errors;
+    // const { account } = this.state;
+
+    // if (account.username.trim() === "")
+    //   errors.username = "Username is required.";
+    // if (account.password.trim() === "")
+    //   errors.password = "Password is required.";
+
+    // return Object.keys(errors).length === 0 ? null : errors;
   };
 
   handleSubmit = (event) => {
