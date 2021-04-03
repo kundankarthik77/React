04117fe4af0b078c7fc0e903e4cc9af84a4a41c3
Kundan Keshidi <kundankarthik77@gmail.com>
2021-04-03 17:44:57 -0500

    render method mistakes corrected

--- first-app/counter-app/vidly/src/components/loginForm.jsx
+++ first-app/counter-app/vidly/src/components/loginForm.jsx
@@ -16,20 +16,19 @@ class LoginForm extends Component {
   // this schema can be seen in npm joi on browser search
   // syntax helps me understand how validations are created
   schema = {
-    username: Joi.string().required().label('Username'),
-    password: Joi.string().required().label('Password'),
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
   };
 
   validate = () => {
-    const options = { abortEarly: false};
+    const options = { abortEarly: false };
     const { error } = Joi.validate(this.state.account, this.schema, options);
 
     if (!error) return null;
 
     const errors = {};
 
-    for(let item of result.error.details) 
-       errors[item.path[0]] = item.message;
+    for (let item of result.error.details) errors[item.path[0]] = item.message;
     return errors;
     // console.log(result);
 
@@ -49,54 +48,54 @@ class LoginForm extends Component {
     event.preventDefault();
 
     const errors = this.validate();
-    this.setState({ errors: errors | {} });
+    this.setState({ errors: errors || {} });
     if (errors) return;
 
     // call the server
     console.log("submitted");
   };
 
-    // if we want to use ref:
-    // const username = this.username.current.value;
-    // console.log(username);
+  // if we want to use ref:
+  // const username = this.username.current.value;
+  // console.log(username);
 
-    // validateProperty = ({ name, value }) => {
-    //   if (name === "username") {
-    //     if (value.trim() === "") return "Username is required.";
-    //   }
+  // validateProperty = ({ name, value }) => {
+  //   if (name === "username") {
+  //     if (value.trim() === "") return "Username is required.";
+  //   }
 
-    //   if (name === "password") {
-    //     if (value.trim() === "") return "Password is required.";
-    //   }
-    // };
-    // Rewriting above code below as follows
-    validateProperty = ({ name, value }) => {
-        const obj = {[name]: value};
-        const schema = { [name]: this.schema[name]};
-        const error = Joi.validate(obj, schema)
-        return error ? error.details[0].message : null;
-    };
+  //   if (name === "password") {
+  //     if (value.trim() === "") return "Password is required.";
+  //   }
+  // };
+  // Rewriting above code below as follows
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const error = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
 
-    handleChange = ({ currentTarget: input }) => {
-      // validation on change
-      const errors = { ...this.state.errors };
-      const errorMessage = this.validateProperty(input);
-      if (errorMessage) errors[input.name] = errorMessage;
-      else delete errors[input.name];
+  handleChange = ({ currentTarget: input }) => {
+    // validation on change
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
 
-      const account = { ...this.state.account };
-      account[input.name] = input.value;
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
 
-      this.setState({ account, errors });
-    };
+    this.setState({ account, errors });
   };
 
   render() {
     const { account, errors } = this.state;
+
     return (
       <div>
         <h1>Login Form</h1>
-        <form onSubmit="this.handleSubmit">
+        <form onSubmit={this.handleSubmit}>
           <Input
             name="username"
             value={account.username}
@@ -111,7 +110,9 @@ class LoginForm extends Component {
             onChange={this.handleChange}
             error={errors.password}
           />
-          <button disabled={this.validate()} className="btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );
